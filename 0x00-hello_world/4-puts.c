fecb143aa3e3600e3cb72f72f13f93e9fdbb8953
Tegegn Kebebaw <tgkebebaw@gmail.com>
2022-06-30 12:32:28 -0700

    please commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(void)
 {
-  puts("Programming is like building a multlingual puzzle\n");
+  puts("Programming is like building a multilingual puzzle\n");
   return(0);
 }
