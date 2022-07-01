aaaf3ba994fcf447fe79153c2e72a6508738ca0e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-01 13:08:42 +0300

    Create 8-print_base16.c

---
+++ 0x00-hello_world/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char hexdc=0x0;
+  for(;hexdc <='f'; hexdc++)
+  {
+  putchar (hexdc);
+  }
+  putchar ('\n');
+	return (0);
+}
