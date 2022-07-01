0282fbefca39c2ad4b061da1c0a9a40a5a53fc4b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-01 10:49:51 +0300

    Create 2-print_alphabet.c

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char apha;
+  for(alpha='a';alpha<='z';alpha++)
+  {
+    putchar(alpha);
+    putchar('\n');
+  }
+	return (0);
+}
