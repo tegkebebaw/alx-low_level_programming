790a71d7291c5719b8dab091c867d9112a93cf9f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-01 21:06:50 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for(n=0;n<10;n++)
+  {
+   putchar(n); 
+    putchar(',');
+    putchar(' ');
+  }
+  putchar('\n');
+	return (0);
+}
