21e4e77fe698ebaf6593867a80dc55cb98c10666
Tegegn Kebebaw <tgkebebaw@gmail.com>
2022-07-01 10:07:40 -0700

    please commit

---
+++ 0x01-variables_if_else_while/3--print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char smal, cap='A';
+  for (smal = 'a'; small <= 'z'; small++)
+  {
+    putchar(small);
+    putchar(cap);
+    putchar('\n');
+    cap++;
+  }
+
+	return (0);
+}
