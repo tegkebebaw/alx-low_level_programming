0292e1882548d2e09133300070f0dce320698f0a
Tegegn Kebebaw <tgkebebaw@gmail.com>
2022-07-01 10:39:30 -0700

    please commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main entry
+ *
+ * betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+for(n = 0; n < 10; n++)
+  {
+   putchar(n); 
+  }
+  putchar('\n');
+ return (0);
+}
