2e4437483af90be195901642fe42b9f84b2796ab
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 21:28:11 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*putchar - print function
+*
+* Return: Always 0 (Success)
+* putchar: print 0 (string)
+*/
+int main(void)
+{
+char smal, cap = 'A';
+for (smal = 'a'; smal <= 'z'; smal++)
+{
+putchar(smal);
+}
+putchar('\n');
+return (0);
+}
