9efe37a9c1d30e2563ea32cf341f9d2b62e12ca0
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 20:30:55 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
 *main entry
+*
 *this is main entry this program just print one message
 */
 int main(void)
@@ -8,6 +9,7 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 /**
 *return for main
+*
 *this is return value for main
 */
 return (0);
