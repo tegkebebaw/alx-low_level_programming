fe0702ca6e85ec8117e02ed369182aa6457745c1
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 20:02:22 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-/*
- *this is simple out put c program using puts
- */
-
+#include <stdio.h>
+/**
+*this is main entry 
+*this program just print one message
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
-return (0);
+ puts("Programming is like building a multilingual puzzle\n");
+ return (0);
 }
