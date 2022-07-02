ee98431cc6c43e99935cf481a4b6ea51e6aa1d68
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 20:04:10 +0300

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*this is main entry 
+*this program just print one message
+*/
+int main(void)
+{
+ printf("Programming is like building a multilingual puzzle\n");
+ return (0);
+}
