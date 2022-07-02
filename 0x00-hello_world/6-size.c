4c763ff34eb7bc29921cd015a5ef67b396248547
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 20:11:43 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*this is main entry 
+*this program just print one message
+*/
+int main(void)
+{
+ printf("Size of a char:%d\n",sizeof(char));
+ printf("Size of a int:%d\n",sizeof(int));
+ printf("Size of a long int:%d\n",sizeof(long));
+ printf("Size of a long long int:%d\n",sizeof(double));
+ printf("Size of a float:%d\n",sizeof(float));
+ return (0);
+}
