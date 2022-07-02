c81525c3ce3f55d10f83667fa42d66569810dd0f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 21:25:12 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*putchar - print function
+*
+* Return: Always 0 (Success)
+* putchar: print 0 (string)
+*/
 int main(void)
 {
-	int n;
-
-	for(n=0;n<10;n++)
-  {
-   putchar(n + '0'); 
-   if (n==9)
-     {
-       continue;
-     }
-   putchar(',');
-    putchar(' ');
-  }
-  putchar('\n');
-	return (0);
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0'); 
+if (n == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
