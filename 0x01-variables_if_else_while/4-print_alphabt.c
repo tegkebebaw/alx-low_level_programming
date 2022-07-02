b6d80d96c2b5faa357bceb6e8c2a00d61386bd4b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-02 21:07:38 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 /**
- *main entry
- *
- * betty style doc for function main goes there 
+* main - Entry point
+*putchar - print function
+*
+* Return: Always 0 (Success)
+* putchar: print 0 (string)
 */
 int main(void)
 {
-	char alpha;
-  for(alpha = 'a'; alpha <= 'z'; alpha++)
-  {
-    if(alpha == 'e' || alpha == 'q')
-    {
-      continue;
-    }
-    else
-    {
-      putchar(alpha);
-    }
-  }
-  putchar('\n');
-  return (0);
+char alpha;
+for(alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if(alpha == 'e' || alpha == 'q')
+{
+continue;
+}
+else
+{
+putchar(alpha);
+}
+}
+putchar('\n');
+return (0);
 }
