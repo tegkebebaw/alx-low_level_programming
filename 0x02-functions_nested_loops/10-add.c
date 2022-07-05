a289f71fb764e3a67918487096079dc8d217cf74
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 23:16:55 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  print_last_digit - calculate absolute value of n.
+ * @n: The number of which the sign will be printed.
+ *_putchar -print character
+ * Return: last digit.
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
