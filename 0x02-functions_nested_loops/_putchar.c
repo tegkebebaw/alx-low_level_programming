fcffbd7daf72857d90ccc2b139ab97f7af8af877
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 21:14:13 +0300

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,13 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
-}
