b922e4b855c1d171b96a0db782a50cbff9b69596
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 22:13:25 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower(int c) - chek if the later is small case or not.
+ *
+ * return(int): return 1 if its small case else 0.
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
