3ec1b39f4ce753ab406f4c34f816482efa9fb734
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 21:36:54 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet
+ * print_alphabet_x10 - prints alphabets 10 times
  *
  * putchar: put char to screen.
  */
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	char letter;
 int i;
-  for (i = 0; i<10; i++)
-  {
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-  }
-	_putchar('\n');
+for (i = 0; i<10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 }
