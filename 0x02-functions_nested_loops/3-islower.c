84f40096d2d274a27017ce14680cabf8251caf10
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 23:31:49 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - chek if the later is small case or not.
- *@c : parameter passed 
- * return: return 1 if its small case else 0.
+ *@c : parameter passed
+ * return (int): return 1 if its small case else 0.
  */
 int _islower(int c)
 {
