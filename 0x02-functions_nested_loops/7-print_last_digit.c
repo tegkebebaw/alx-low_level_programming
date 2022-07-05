68f1b8887412600ba7efeea3dcd232f440b3b01e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 22:54:39 +0300

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  print_last_digit - calculate absolute value of n.
+ * @n: The number of which the sign will be printed.
+ *_putchar -print character
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
