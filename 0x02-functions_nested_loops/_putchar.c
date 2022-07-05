cdccbf14464233e47c0e119c4a798f97b525288e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 21:08:31 +0300

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -13,7 +12,12 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-int print-alphabet(void)
+void print_alphabet(void)
 {
-	return (puts("abcdefghijklmnopqrstuvwxyz"));
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
