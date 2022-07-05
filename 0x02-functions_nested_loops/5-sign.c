0df7b6b63f5dac6aee7e1cfeb726b040cb4de11d
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 22:24:21 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int print_sign(int n)
+{
+if (n>0)
+{
+return (1);
+}
+else if ()
+{
+return (0);
+}
+else
+{
+return (0); 
+}
+}
