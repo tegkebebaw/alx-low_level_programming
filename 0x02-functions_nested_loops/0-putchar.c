6919bcea74107895c737ac0371033edd29acaeb4
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-05 19:18:24 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*printf - print function
+*
+* Return: Always 0 (Success)
+* printf: print 0 (string)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
