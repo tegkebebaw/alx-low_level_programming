d4f5ce6b51aa9275da2229322237a8afa9bee9ad
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:36:46 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - chek if the later is small case or not.
+ *@a : parameter 1 passed
+ *@b : parameter 2 passed
+ * return (int): return 1 if its capital otherwise 0.
+ */
+int int mul(int a, int b)
+{
+return (a * b);
+}
