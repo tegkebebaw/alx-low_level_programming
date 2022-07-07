674945bfc885786bc908d6ce0465fd71a254dae2
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 17:18:20 +0300

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * return - return 0 allways
+ */
+void print_triangle(int size)
+{
+int i,j;
+for (i = 0; i < size; i++)
+{
+  for (j = size; j > i; j--)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n'); 
+}
+}
