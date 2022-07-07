2ce9299fb09ab9de959e1da047070c5161a06aac
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 17:30:08 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - chek if the later is small case or not.
  * 
@@ -10,7 +9,7 @@ void print_numbers()
 int i;
 for (i = 0; i < 10; i++)
 {
-printf("%d", i);
+_putchar(i+'0');
 }
-printf("\n");
+_putchar ("\n");
 }
