2dc39b31c0f8ed4861e412e0f8cf2062c1aacee4
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 19:31:41 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _isdigit - chek if the later is small case or not.
-*@c : parameter passed
-*
-*return (int): return 1 if its digit otherwise 0.
-*/
+ * _isdigit - chek if the later is small case or not.
+ *@c : parameter passed
+ *
+ *return (int): return 1 if its digit otherwise 0.
+ */
 int _isdigit(int c)
 {
 if (c >= 48 && c <= 57)
