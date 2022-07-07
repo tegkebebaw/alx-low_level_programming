4b6e696ddad8752e877e91e05e4e9c09e4228c27
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:51:37 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - chek if the later is small case or not.
+ * 
+ * return (int): return 1 if its capital otherwise 0.
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+j=0;
+for (; j < 15; j++)
+{
+printf("%d", i);
+}
+printf("\n");
+}
+printf("\n");
+}
