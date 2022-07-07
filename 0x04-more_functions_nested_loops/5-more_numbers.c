89fa02a3b5dc3566225d1edbce49f2cfd94eaec7
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 17:27:32 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_numbers - chek if the later is small case or not.
  * 
@@ -13,9 +12,9 @@ for (i = 0; i < 10; i++)
 j=0;
 for (; j < 15; j++)
 {
-printf("%d", i);
+_putchar(i+'0');
 }
-printf("\n");
+_putchar("\n");
 }
-printf("\n");
+_putchar("\n");
 }
