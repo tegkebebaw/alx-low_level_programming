aab0b529d55b4c4114c6110fdac8355135027994
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 20:20:00 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int mul(int a, int b);
 int _isdigit(int c);
 void print_numbers(void);
 void print_most_numbers(void);
+int _putchar(char c);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
