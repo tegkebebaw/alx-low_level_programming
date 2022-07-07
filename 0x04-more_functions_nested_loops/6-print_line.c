98358178923fdd30583307931fe655e13a22c268
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:53:56 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - chek if the later is small case or not.
+ * 
+ */
+void void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+putchar('_');
+}
+printf("\n");
+}
