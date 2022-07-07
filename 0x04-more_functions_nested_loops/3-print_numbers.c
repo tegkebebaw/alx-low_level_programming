4a750e99f7c15f43a17b7d6c0a8fa7788f980397
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:44:21 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,15 @@
-
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - chek if the later is small case or not.
+ * 
+ * return (int): return 1 if its capital otherwise 0.
+ */
+void print_numbers()
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+}
