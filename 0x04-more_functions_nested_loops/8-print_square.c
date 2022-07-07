3a2ada847832167713ef457560aee81701e3dd7b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 21:00:18 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
  * print_square - chek if the later is small case or not.
- * 
+ * @size - parameter passed
+ *@i - initial variable
+ *@j - variable
  */
 void print_square(int size)
 {
 int i, j;
 for (i = 0; i < size; i++)
 {
-j=0;
+j = 0;
 for (; j < size; j++)
 {
 _putchar('#');
