9218f98bd17f0e49200c74d1df683fa8c1e9880a
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:48:05 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - chek if the later is small case or not.
+ * 
+ * return (int): return 1 if its capital otherwise 0.
+ */
+void print_numbers()
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 4 || i == 2)
+{
+continue
+}
+else
+printf("%d", i);
+}
+printf("\n");
+}
