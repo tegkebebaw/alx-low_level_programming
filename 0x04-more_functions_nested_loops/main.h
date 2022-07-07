9c1088042499024bfa33af46ce548f7c451b82b0
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-07 16:11:42 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,11 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+int isupper (int c);
 
+#endif
 
-
-int isupper (int c)
