442ea5cd9c1ddfc0bd9059669643bb3e62ccfe80
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-08 20:12:02 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,19 +3,20 @@
  * print_diagonal - chek if the later is small case or not.
  *@i - initial variable
  *@j - temp variable
+ *@n - length of line to be draw
  */
 void print_diagonal(int n)
 {
 int i, j;
 for (i = 0; i < n; i++)
 {
-j = 0;
-for (; j < i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
 }
+if (n == 0)
 _putchar('\n');
 }
