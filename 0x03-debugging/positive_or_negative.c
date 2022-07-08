55011a1aeacaf98374485c8caca8e4b9a5a4c2fe
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-08 19:00:25 +0300

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,23 +3,21 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+positive_or_negative(int i)
 {
-	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n<0)
+	if (i<0)
 	{
-	printf("%d is negative", n);
+	printf("%d is negative", i);
 	}
-	else if (n>0)
+	else if (i>0)
 	{
-	printf("%d is positive", n);
+	printf("%d is positive", i);
 	}
 	else
 	{
-	printf("%d is zero", n);
+	printf("%d is zero", i);
 	}
 	return (0);
 }
