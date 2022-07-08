370830294c4afa53c7a1678158cbaf23408ade62
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-08 19:06:13 +0300

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 void positive_or_negative(int i)
 {
-	if (i<0)
-	{
-	printf("%d is negative", i);
-	}
-	else if (i>0)
-	{
-	printf("%d is positive", i);
-	}
-	else
-	{
-	printf("%d is zero", i);
-	}
+if (i<0)
+{
+printf("%d is negative\n", i);
+}
+else if (i>0)
+{
+printf("%d is positive\n", i);
+}
+else
+{
+printf("%d is zero\n", i);
+}
 }
