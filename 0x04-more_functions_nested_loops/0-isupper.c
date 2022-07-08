a48e7254146674eb293c95e3f48048b84ced1f8c
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-08 19:50:47 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * isupper - chek if the later is small case or not.
+ * _isupper - chek if the later is small case or not.
  *@c : parameter passed
  *
  * return (int): return 1 if its capital otherwise 0.
  */
 int _isupper (char c)
 {
-if (c >= 'A' && c <= 'Z')
+if ((c >= 'A') && (c <= 'Z'))
 return (1);
 else
 return (0);
