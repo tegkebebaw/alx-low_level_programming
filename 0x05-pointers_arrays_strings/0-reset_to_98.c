815203bd3326f69a55fa812a075c8164be376bde
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-11 20:46:45 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - reset a number
- *n - is apointer parameter passed to be reset.
+ *@n - is apointer parameter passed to be reset.
  */
 void reset_to_98(int *n)
 {
