d5a2158ef5a97d14e955a5565d0ae0680e06d5bf
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-11 21:19:56 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+    write(1, str, 1));
+}
