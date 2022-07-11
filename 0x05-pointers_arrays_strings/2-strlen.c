c7dd14f5c902d3340b8a7bc39baad193186eed6e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-11 22:13:22 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,8 +4,7 @@
  * _strlen - length of
  *return : return length of character
  * @s : parameter 1.
- *return - return length of character
- *strlen : is function to determine length of string
+ *return : is function to determine length of string
  */
 int _strlen(char *s)
 {
