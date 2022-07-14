c6c6aefb342c6a02900273e7f832805f3d8be18b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-14 18:38:47 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies string from src to des,
+ * @dest: destination.
+ * @src: source.
+ * @n: number of character to be concatinated.
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int count = 0, count2 = 0;
+while (count >= 0)
+{
+if (*(src + count) == '\0')
+break;
+count++;
+}
+while (count2 >= 0)
+{
+if ((*(src + count) == '\0' ) || (count2 > n))
+break;
+*(dest + count) = *(src + count);
+count++;
+count2++;
+}
+return (dest);
+}
