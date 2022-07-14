56996869bf424a4a653b66825e29de58a6360f26
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-14 19:40:39 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - compair string s1 and s2,
+ *@s2: destination.
+ *@s2: source.
+ *Return: the integer value.
+ */
+void reverse_array(int *a, int n)
+{
+int *i, j, nn = 0;
+for (j = n-1; j > 0; j--)
+{
+  i[nn] = a[j];
+  i++;
+}
+return (i);
+}
