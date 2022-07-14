4f7d8c406368d4950adea7a5653e3e5b8b09b003
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-14 19:43:36 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_strcmp - compair string s1 and s2,
- *@s2: destination.
- *@s2: source.
- *Return: the integer value.
+ *reverse_array - compair string s1 and s2,
+ *@a: destination.
+ *@n: source.
  */
 void reverse_array(int *a, int n)
 {
 int *i, j, nn = 0;
 for (j = n-1; j > 0; j--)
 {
-  i[nn] = a[j];
-  i++;
+i[nn] = a[j];
+i++;
 }
-return (i);
 }
