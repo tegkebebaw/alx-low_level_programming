111d2ad1a420b5698472ce73508c8f029fef68c4
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-14 19:18:55 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - compair string s1 and s2,
+ *@s2: destination.
+ *@s2: source.
+ *Return: the integer value.
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(*s1, *s2));
+}
