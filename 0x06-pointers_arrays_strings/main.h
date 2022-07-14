69cce607ea9985630450157b46e9854cc50ecb7f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-14 18:27:12 +0300

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+
+#endif
