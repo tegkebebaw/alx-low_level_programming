7d05397a8efa00643b3a336702a81898564070f5
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-15 18:59:25 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,17 @@ char *_strncat(char *dest, char *src, int n)
 int count = 0, count2 = 0;
 while (count >= 0)
 {
-if (*(src + count) == '\0')
+if (*(dest + count) == '\0')
 break;
 count++;
 }
 while (count2 >= 0)
 {
-if ((*(src + count) == '\0' ) || (count2 > n))
+if ((*(src + count2) == '\0') || (count2 > n))
 break;
-*(dest + count) = *(src + count);
+*(dest + count) = *(src + count2);
 count++;
 count2++;
-}    
+}
 return (dest);
 }
