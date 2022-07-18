6632659dde7a3bba9f009a16c72339d07a80edac
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 21:24:13 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,14 +8,20 @@
 char *_strstr(char *haystack, char *needle)
 {
 unsigned int i, j;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-				return (s + i);
-		}
-	}
-	return ('\0');
+char *bhaystack;
+char *pneedle;
+while (*haystack != '\0')
+{
+bhaystack = haystack;
+pneedle = needle;
+while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+{
+haystack++;
+pneedle++;
+}
+if (!*pneedle)
+return (bhaystack);
+haystack = bhaystack + 1;
+}
+return (0);
 }
