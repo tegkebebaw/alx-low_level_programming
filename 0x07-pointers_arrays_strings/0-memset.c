ed12e5c90d5e0b4d904f4fad048564a948ff1831
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 19:52:59 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *_memset - fill memory with constatns,
  *@s: memory to write into.
@@ -9,8 +8,9 @@
  */
 char *_memset(char *s, char b, unsigned int n);
 {
-int i;
+unsigned int i;
 for (i = 0; i < n; i++)
 *(s + i) = b;
+  
 return (s);
 }
