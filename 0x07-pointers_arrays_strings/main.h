4da805f034477f7c9f358c283fd48c9f36ec5c94
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 19:49:38 +0300

    Create main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
