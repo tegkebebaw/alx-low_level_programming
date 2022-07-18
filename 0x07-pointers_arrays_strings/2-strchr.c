bbb7f7c0b93f02c64483d5ed129c091f3f0605c3
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 20:24:32 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,15 @@
 char *_strchr(char *s, char c)
 {
 int i=0;
-
+bool st=false;
 while (*(s + i) != '\0')
 {
 if (*(s + i) == c)
+bool=true;
 break;
 }
+if(bool)
 return (s + i);
+else
+return('\0');
 }
