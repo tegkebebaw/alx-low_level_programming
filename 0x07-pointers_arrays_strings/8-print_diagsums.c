d246852198240e3d3b3a445dc1de514dbe0ceb6f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 21:50:05 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_chessboard - print chess board,
+ *@a: two dimensional array parameter passed.
+ *Return: return 0.
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum = 0;;
+for (i = 0; *(a + i) != '\n'; i += size)
+{
+sum += *(a + );
+}
+printf("%d", sum);
+}
+}
