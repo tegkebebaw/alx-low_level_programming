9391962e15b15bfe47dd218f3c9e56d4a568e34b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 20:01:30 +0300

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory,
+ *@dest: memory destination.
+ *@src: source to copied from.
+ *@n: length of memory area to be copied.
+ *Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+*(s + dest) = *(src+1);   
+return (dest);
+}
