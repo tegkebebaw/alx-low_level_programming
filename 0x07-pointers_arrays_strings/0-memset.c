a4d64cd5f2e99f0665d8ad601d5dcc46df15819b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 19:45:07 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memset - fill memory with constatns,
+ *@s: memory to write into.
+ *@b: character to fill in.
+ *@n: length of memory area to fill.
+ *Return: the pointer to dest.
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+int i;
+for (i = 0; i < n; i++)
+*(s + i) = b;
+return (*s);
+}
