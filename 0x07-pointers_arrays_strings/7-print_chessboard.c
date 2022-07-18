f05f7cce6adc40ffa5127596af5d29f9571bdd6a
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 21:40:28 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,10 +7,10 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i,j;
-for (i = 0; i<8; i++)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-for (j = 0; j<8; j++)
+for (j = 0; j < 8; j++)
 {
 printf("%c", a[i][j]);
 }
