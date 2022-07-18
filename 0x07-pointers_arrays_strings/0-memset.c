62263009941face3ac91f661b1dd24baaf3ea43e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-18 19:53:38 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  *@n: length of memory area to fill.
  *Return: the pointer to dest.
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
