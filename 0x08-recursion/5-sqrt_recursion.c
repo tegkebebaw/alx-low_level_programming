238170dca637df1c5b1d90a97568071d8ec51e4c
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-20 19:58:36 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * powerof - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
+ */
+int powerof(int n, int y)
+{
+	if (c % (n / y) == 0)
+	{
+		if (y * (n / y) == n)
+			return (y);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, y + 1));
+}
+/**
+ * _sqrt_recursion - find factorial of a number.
+ * @n: The parameter 1
+ *Return: return square root of parameter passed.
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (0);
+else if (n == 1)
+return (1);
+else
+return (powerof(n, 2));
+}
