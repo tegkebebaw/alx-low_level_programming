caeca2a0decfabcf892a13227e4cd3e642ae238e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-20 17:04:31 +0300

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - print a string passed and end with new line.
+ * @s: The string passed to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+int i;
+for(i = 0; *(s + i) != '\0'; i++)
+printf("%c", (s + i));
+printf("\n");
+}
