5feb41a72eecd3bf8c869b66f64b64c29353952f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-20 17:16:47 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print a string passed in revers.
+ * @s: The string passed to print
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+int i = strlen(s), j;
+for(; i>0; i--)
+{
+printf("%c\n", *(s-i));
+}
+printf("\n");
+}
