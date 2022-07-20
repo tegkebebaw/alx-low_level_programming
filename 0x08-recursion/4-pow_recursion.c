ed89e3cbdfd679ef29410ef11b38173bd9947aee
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-20 19:41:50 +0300

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - find factorial of a number.
+ * @x: The parameter 1
+ *@y: parameter 2
+ *Return: return xpower of y.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (n == 0)
+return (0);
+else
+return (x + factorial(y - 1));
+}
