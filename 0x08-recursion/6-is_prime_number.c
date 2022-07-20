5896e35daf2ae182726936ecdcf0652c4296a024
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-20 20:15:27 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (-1);
-else if (n <= 1)
+else if (n <= 3)
 return (1);
 else
 {
-if(((n % 2) == 0 )|| ((n % 3) == 0))
+if((n % 2 == 0 )|| (n % 3 == 0))
 return (0);
 else
 return (-1);
