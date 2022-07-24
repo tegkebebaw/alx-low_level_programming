5295762cc29b252d05cc09686b66ab458991d765
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-24 11:23:48 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@arc - parameter 1
+ *@argv - parameter lists
+ *return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+  int i;
+for(i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
