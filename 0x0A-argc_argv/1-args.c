84eac043bd21b00d3a62b4d05d1ae6a3537de938
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-24 11:19:49 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@arc - parameter 1
+ *@argv - parameter lists
+ *return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
