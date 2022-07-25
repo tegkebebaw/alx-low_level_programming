e74169432636c2ff6227e36f559c77142a8ba252
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-25 18:42:57 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 int i, sum = 0, j;
 for (i = 1; i < argc; i++)
 {
-for (j = 0; i < argv[i][j]; j++)
+for (j = 0; j < argv[i][j]; j++)
 {
 if (!isdigit(argv[i][j]))
 {
