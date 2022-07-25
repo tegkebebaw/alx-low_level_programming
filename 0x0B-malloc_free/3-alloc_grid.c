c6f57d8d377b5c4f807c6211e3aaf14b3d1cb823
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-25 21:22:40 +0300

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - concatinate two string
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer to two dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+if (width == 0 || height == 0)
+return (NULL);
+else
+{
+int **arry = (int *)calloc(height,sizeof(int)*width );
+if (arry == NULL)
+return (NULL);
+else
+return (arry);
+}
+}
