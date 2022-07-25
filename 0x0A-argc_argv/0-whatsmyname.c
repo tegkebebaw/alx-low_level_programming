d348839d7a022bad0d6544d99e9cf3968bca55c1
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-25 18:08:55 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - entry point
- *@arc - parameter 1
+ *@argc - parameter 1
  *@argv - parameter lists
  *return: Always 0 (success)
  */
@@ -10,6 +10,6 @@ int main(int argc, char *argv[])
 if (argc > 0)
 printf("%s\n", argv[0]);
 else
-printf("%s\n", argv[0]); 
+printf("%s\n", argv[0]);
 return (0);
 }
