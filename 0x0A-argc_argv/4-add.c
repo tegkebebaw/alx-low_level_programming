1a3b1cb9e356b16c3b67884c38e8ad3ad99dfbf4
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-25 18:40:51 +0300

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *@argc : parameter 1
+ *@argv : parameter lists
+ *Return: Always 0 (success) or 1 failed
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0, j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; i < argv[i][j]; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum = sum + atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
