716497c64a059f717d12f1bed6f93b510e21c505
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-07-28 19:57:51 +0300

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range -  memory for an array.
+ * @min: min value.
+ * @max: max value.
+ *
+ * Return: pointer to the allocated array.
+ */
+int *array_range(int min, int max)
+{
+int *c;
+unsigned int i, j;
+  
+if (min > max)
+return (NULL);
+j = max - min;
+c = malloc(sizeof(int) * j);
+if (c == NULL)
+return (NULL);
+for (i = 0; i < j; i++)
+c[i] = min + i;
+return (c);
+}
