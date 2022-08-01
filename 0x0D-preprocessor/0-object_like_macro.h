44f0164ea76088f6a6933879650143afa8e2e344
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:15:21 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,11 @@
-/**
- * SIZE - define static variable named SIZE with value 1024
- *
- */
-#ifendif _SIZE_
-#define _SIZE_
 
+#ifndef 0-object_like_macro_H
+#define 0-object_like_macro_H
+/**
+*File: 0-object_like_macro.h
+*
+*descriptio: Header file containing definition for SIZE
+*/
 #define SIZE 1024
 
 #endif
