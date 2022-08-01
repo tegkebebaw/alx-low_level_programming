83fc8e674a75e3a57419c335f7a21ddf417937b7
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:56:32 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,9 @@
 #define dog_H
 /**
 *File: dog.h
-*
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
 *descriptio: Header file containing declarations user defined variable type dog
 */
 struct dog
