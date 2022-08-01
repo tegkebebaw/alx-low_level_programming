e57c25423bc183b152a8d98a5a10e937745ac36b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:08:26 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifendif _3-function_like_macro_
+#ifndif _3-function_like_macro_
 #define _3-function_like_macro_
 
-#define ABS(x) ((x) >= (0) ? (x):(-1 * (x)))
+#define ABS(x) ((x) >= (0) ? (x):((-1) * (x)))
 
 #endif
