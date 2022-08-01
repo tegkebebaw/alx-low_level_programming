40ef5effb0a09c4ab0e1f1c02ab25456983e7e2f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:34:43 +0300

    Create main.h

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
