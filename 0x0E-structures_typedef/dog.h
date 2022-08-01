d9615832aa1e2b4564c1cb6fb38d2a9cfe43f129
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:26:52 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_H
+#define dog_H
+/**
+*File: dog.h
+*
+*descriptio: Header file containing declarations user defined variable type dog
+*/
+struct dog
+{
+  char *name;
+  float age;
+  char * owner;
+};
+
+#endif
