98bfa8086fdc884659a9ddf611b82348895ec980
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:13:00 +0300

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,10 @@
-#ifendif _4-sum_
-#define _4-sum_
-
+#ifndif 4-sum_H
+#define 4-sum_H
+/**
+*File: 4-sum.h
+*
+*descriptio: Header file containing definition for SUM(x,y)
+*/
 #define SUM(x, y) ((x) + (y))
 
 #endif
