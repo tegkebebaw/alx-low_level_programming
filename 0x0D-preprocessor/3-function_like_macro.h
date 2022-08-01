7fed2a1bfb0401ed232cac4c3f742a8c1129f787
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:11:33 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,10 @@
-#ifndif _3-function_like_macro_
-#define _3-function_like_macro_
-
+#ifndif 3-function_like_macro_H
+#define 3-function_like_macro_H
+/**
+*File: 3-function_like_macro.h
+*
+*descriptio: Header file containing definition for ABS(x)
+*/
 #define ABS(x) ((x) >= (0) ? (x):((-1) * (x)))
 
 #endif
