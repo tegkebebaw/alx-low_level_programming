6ba0705d69c45c36a71498f6bf36f72c819da100
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:46:13 +0300

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - print dog atribute
+ *@d: dog object passed as parameter
+ */
+void print_dog(struct dog *d)
+{
+if(d == NULL)
+printf("(nil)\n");
+else
+{
+if(d.name == NULL)
+printf("Name: (nil)\n");
+else
+printf("name:%s\n", d.name);
+if(d.age == NULL)
+printf("age: (nil)\n");
+else
+printf("age:%d\n", d.age);
+if(d.owner == NULL)
+printf("owner: (nil)\n");
+else
+printf("owner:%s\n", d.owner);
+}
+}
