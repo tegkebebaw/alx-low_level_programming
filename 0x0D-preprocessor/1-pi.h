659ef5466e03ed0c44971c7068d59eeb51f7a9bd
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-01 20:05:35 +0300

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,10 @@
-#ifendif _PI_
-#define _PI_
-
+#ifendif _1-pi_
+#define _1-pi_
+/**
+*File: 1-pi.h
+*
+*description: header file containing definintion for pi
+*/
 #define PI 3.14159265359
 
 #endif
