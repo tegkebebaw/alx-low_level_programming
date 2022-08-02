d22ffc664f822da6536d9acd785e295f536d57d4
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-02 14:24:34 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,17 @@ if(d == NULL)
 printf("(nil)\n");
 else
 {
-if(d.name == NULL)
+if(d->name == NULL)
 printf("Name: (nil)\n");
 else
-printf("name:%s\n", d.name);
-if(d.age == NULL)
+printf("name:%s\n", d->name);
+if(d->age == NULL)
 printf("age: (nil)\n");
 else
-printf("age:%d\n", d.age);
-if(d.owner == NULL)
+printf("age:%d\n", d->age);
+if(d->owner == NULL)
 printf("owner: (nil)\n");
 else
-printf("owner:%s\n", d.owner);
+printf("owner:%s\n", d->owner);
 }
 }
