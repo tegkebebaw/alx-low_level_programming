92f6dbedd7a301b9ff0a9f17a55a4e432743d669
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-02 13:58:39 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,10 @@
  *@name: name index value
  *@age: age index value
  *@owner: owner index value
- * Return: pointer to allocated size if good.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if(d)
+if (d)
 {
 d ->name = name;
 d ->age = age;
