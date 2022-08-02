a9b87d00f8375c5cd56f94badceac4fe77e34f3b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-02 10:56:38 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include "dog.h"
 /**
- * malloc_checked - check the code
- *@b: size of memory to be allocated
+ * init_dog - initialize
+ *@d: structure passed
+ *@name: name index value
+ *@age: age index value
+ *@owner: owner index value
  * Return: pointer to allocated size if good.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-*(d.name) = name;
-*(d.age) = age;
-*(d.owner) = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
