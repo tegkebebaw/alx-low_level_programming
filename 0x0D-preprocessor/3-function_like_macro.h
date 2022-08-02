a387c2a48b84cc04effc1ca9aefd9d9fe9238bdb
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-02 10:38:27 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,8 @@
 #define like_macro_H
 /**
 *File: 3-function_like_macro.h
-*
+*@x: parameter passed.
+*ABS:definition to calculate absolute value
 *descriptio: Header file containing definition for ABS(x)
 */
 #define ABS(x) ((x) >= (0) ? (x):((-1) * (x)))
