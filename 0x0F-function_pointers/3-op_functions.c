ac2f2ae76c256844980c33ce579cb6fce57662ba
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-03 20:25:39 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,21 +1,66 @@
-
+#include "3-calc.h"
+/**
+ * op_add - adds two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: sum.
+ */
 int op_add(int a, int b)
 {
   return a + b;
 }
+/**
+ * op_sub - subtract two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: difference.
+ */
 int op_sub(int a, int b)
 {
   return a - b;
 }
+/**
+ * op_mul - multiply two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: product.
+ */
 int op_mul(int a, int b)
 {
   return a * b;
 }
+/**
+ * op_div - adds two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: devion.
+ */
 int op_div(int a, int b)
 {
+  if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
   return a / b;
 }
+/**
+ * op_mod - find mode of two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ *
+ * Return: mod.
+ */
 int op_mod(int a, int b)
 {
+  if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
   return a % b;
 }
