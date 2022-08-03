ecb5f098f4ea05f77435ba0a3a778f4a2fc4f555
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-03 20:03:26 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,8 +9,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 if (size <= 0)
 return (-1);
-size_t i;
-for (i = 0; i < size; i++)
+int i = 0;
+for (i; i < size; i++)
 {
 if (cmp(array[i]))
 return (i);
