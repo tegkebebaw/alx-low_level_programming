109c1e2790090bca1a20950d456271475b66b884
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-03 17:15:04 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,11 @@
 #ifndef function_pointer
 #define function_pointer
 
+/**
+*File: function_pointer.h
+*
+*descriptio: Header file containing declarations for all function
+*/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
