b80b88fbbebeaab00116490e8c88f08b54e66b15
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-03 19:49:15 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 /**
  * print_name - a function which points to another function whch prints the string passed
- *
+ *@name: string to be printed
+ *@f pointer to function which print name
  * Return: return nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+f(name);
 }
