f491945a24888a910e6a992ebeb444abe1dd8fa7
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-03 16:39:19 +0300

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * array_iterator - a function which points to another function whch can do any thing in the given function
+ *
+ * Return: return nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+  int i;
+ for (i = 0; i < size; i++)
+ {
+    action(array[i]);
+ }
+}
