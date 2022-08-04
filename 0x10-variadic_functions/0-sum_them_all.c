ccefda28c276b59d6b87be12280d5bce9a80fca7
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-04 19:05:46 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-  if ( n == 0)
-    return 0;
-  else
-  {
-  va_list myary;
-  unsigned int i, sum;
-  va_start (myary, n);
-  sum = 0;
-  for (i = 0; i < n; i++)
-    sum += va_arg (myary, int);
-
-  va_end (myary);
-  return sum;
-  }
+if ( n == 0)
+return 0;
+else
+{
+va_list myary;
+unsigned int i, sum;
+va_start (myary, n);
+sum = 0;
+for (i = 0; i < n; i++)
+sum += va_arg (myary, int);
+va_end (myary);
+return sum;
+}
 }
