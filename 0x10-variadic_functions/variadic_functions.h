11823b0915c0706c1f0f4395ac00ca462c57e9af
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-04 18:58:03 +0300

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#ifndef variadic_functions
+#define variadic_functions
+/**
+*File: variadic_functions.h
+*
+*description: Header file containing declarations for all function
+*/
+int sum_them_all(const unsigned int n, ...);
+
+#endif
