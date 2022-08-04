7749ba65b23094631839c38f2857ce196781cb0a
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-04 18:55:36 +0300

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function to return sum of numbers passed
+ *@n : number of parameter passed
+ *... : list of parameter passed
+ * Return: Always sum or 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+  if ( n == 0)
+    return 0;
+  else
+  {
+  va_list myary;
+  int i, sum;
+  va_start (myary, n);
+  sum = 0;
+  for (i = 0; i < n; i++)
+    sum += va_arg (myary, int);
+
+  va_end (myary);
+  return sum;
+  }
+}
