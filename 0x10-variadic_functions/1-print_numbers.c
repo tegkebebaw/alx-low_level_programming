e3421734f0d97a58fbec5fd05c4c7390f19525dd
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-04 19:20:27 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,21 +9,15 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-if ( n == 0)
-return 0;
-else
-{
 va_list myary;
 unsigned int i;
 va_start (myary, n);
-sum = 0;
 for (i = 0; i < n; i++)
 {
 if (separator)
-printf("%d%c", va_arg (myary, int), separator);
+printf("%d%s ", va_arg (myary, int), separator);
 else
 printf("%d", va_arg (myary, int));
 }
-printf("\n"r);
-}
+printf("\n");
 }
