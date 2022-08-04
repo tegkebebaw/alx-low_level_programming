4d7149d46ee6d9b177580f156fa14c8d0b89b3da
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-04 19:15:15 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,6 @@
 *description: Header file containing declarations for all function
 */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
