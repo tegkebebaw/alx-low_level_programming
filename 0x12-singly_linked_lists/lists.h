dc0ebb64bd3206ada685c064f98ba1efaf437de6
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-11 19:50:13 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,6 @@
 #ifndef lists_H
 #define lists_H
 /**
-*File: lists.h
-*
-*description: Header file containing declarations for all function
-*/
-/**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
