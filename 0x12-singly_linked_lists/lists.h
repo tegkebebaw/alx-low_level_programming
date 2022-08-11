aa3c3fab2c501280c08f860d82a8338c80fdb10f
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-11 20:04:02 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,8 @@
  * Description: conatan definitions
  */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
