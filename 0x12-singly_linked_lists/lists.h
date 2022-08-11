6176e12a9680d4c52e065f58e6b48e00c89c273b
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-11 19:41:02 +0300

    Create lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef lists_H
+#define lists_H
+/**
+*File: lists.h
+*
+*description: Header file containing declarations for all function
+*/
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+#endif
