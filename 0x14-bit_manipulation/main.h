206eea732168479a448f228fdf848976fe30cdda
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-18 14:45:08 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@
 *descriptio: Header file containing declarations for all function
 */
 unsigned int binary_to_uint(const char *b);void _print_rev_recursion(char *s);
+int _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif
