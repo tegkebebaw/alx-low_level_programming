df1228ee3d11f2eed8a68751c4c977405716dfd0
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-18 14:36:12 +0300

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number;
+	int i = 0, b2;
+
+	if (!b)
+		return (0);
+
+	number = 0;
+  while(b[i] != '\0')
+  {
+  i++;
+  }
+	for (i--, b2 = 1; i >= 0; i--, b2 *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			number += b2;
+		}
+	}
+
+	return (number);
+}
