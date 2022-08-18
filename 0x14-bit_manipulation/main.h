7fcc4885fab21f883f5a78597ab167d0e0a7b618
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-18 14:38:30 +0300

    Create main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+unsigned int binary_to_uint(const char *b);void _print_rev_recursion(char *s);
+
+#endif
