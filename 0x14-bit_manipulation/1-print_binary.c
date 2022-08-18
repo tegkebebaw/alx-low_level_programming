571c1a7d3ce865646fe978389ab4531f1754942e
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-18 14:43:07 +0300

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - convert base 10 to base 2 number
+ * @n: parameter passed in base 10.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
