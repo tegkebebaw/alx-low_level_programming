69d448c59f603233d769b43c08bd577e47b054b6
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-22 21:18:21 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,5 +6,6 @@
 *descriptio: Header file containing declarations for all function
 */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
