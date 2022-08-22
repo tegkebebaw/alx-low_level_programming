f6d1c26a3b37f6858685bfefa419dc3edbb604e0
tegkebebaw <86592774+tegkebebaw@users.noreply.github.com>
2022-08-22 21:16:10 +0300

    Create main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef main_H
+#define main_H
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
